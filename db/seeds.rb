ef497c889f188fa9d245dfe80de50acf1b25eb88
Jay Arisah <gift_ij2002@yahoo.com>
2019-11-22 08:01:24 +0100

    Depot scaffold

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+Product.find_or_create_by!(title: 'Build Chatbot Interactions', description: %{<p><em>Responsive, Intuitive Interfaces with Ruby</em></p>}, image_url: 'dpchat.jpg', price: 20.00)
+
+Product.find_or_create_by!(title: 'Build Missy Interactions', description: %{<p><em>Responsive, Intuitive Interfaces with Ruby</em></p>}, image_url: 'dpchat.jpg', price: 40.00)
+
+Product.find_or_create_by!(title: 'Build haha Interactions', description: %{<p><em>Responsive, Intuitive Interfaces with Ruby</em></p>}, image_url: 'dpchat.jpg', price: 60.00)
