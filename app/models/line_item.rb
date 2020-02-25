3c8da6c541feb379215e4cd62090b1ca3e03efb8
Jay Arisah <gift_ij2002@yahoo.com>
2020-02-25 12:41:19 -0100

    customers can checkout

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,8 +1,15 @@
 class LineItem < ApplicationRecord
-  belongs_to :product
+  before_validation :add_price
+
+  belongs_to :product, optional: true
+  belongs_to :order, optional: true
   belongs_to :cart
 
   def total_price
     product.price * quantity
+  end
+
+  def add_price
+    self.price = product.price
   end
 end
