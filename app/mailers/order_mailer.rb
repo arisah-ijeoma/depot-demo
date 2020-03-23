329716614d702fd17c778c9844fbd3f24711433e
Jay Arisah <gift_ij2002@yahoo.com>
2020-03-23 14:27:22 +0100

    add mailer

---
+++ app/mailers/order_mailer.rb
@@ -0,0 +1,13 @@
+class OrderMailer < ApplicationMailer
+  default from: 'Jay Jay <jay@example.com'
+
+  def received(order)
+    @order = order
+    mail to: order.email, subject: 'Pragmatic Store Order Confirmation'
+  end
+
+  def shipped(order)
+    @order = order
+    mail to: order.email, subject: 'Pragmatic Store Order Shipped'
+  end
+end
